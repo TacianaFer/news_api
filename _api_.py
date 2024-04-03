1feff4995ce4345497d2689b2da44412ac15183d
levyvix <levy.vix@gmail.com>
2024-04-03 19:23:27 -0300

    api funcionando 2 rotas

--- _api_.py
+++ _api_.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify
 import pandas as pd
-from sqlalchemy import create_engine
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///news_api/news.db')
+engine = create_engine("sqlite:///news_api/news.db")
+
 
 # Contar as notícias por ano, mês e dia de publicação
-@app.route('/count_by_date')
+@app.route("/count_by_date")
 def count_by_date():
     query = """
         SELECT strftime('%Y', publishedAt) as year,
@@ -17,22 +18,27 @@ def count_by_date():
         FROM news
         GROUP BY year, month, day
     """
-    result = engine.execute(query).fetchall()
+
+    with engine.connect() as con:
+        result = con.execute(text(query)).fetchall()
+    # result = engine.execute(query).fetchall()
     data = [{"year": row.year, "month": row.month, "day": row.day, "count": row.count} for row in result]
     return jsonify(data)
 
+
 # Contar as notícias por fonte e autor
-@app.route('/count_by_source_author')
+@app.route("/count_by_source_author")
 def count_by_source_author():
     query = """
-        SELECT source, author, COUNT(*) as count
+        SELECT source_name, author, COUNT(*) as count
         FROM news
-        GROUP BY source, author
+        GROUP BY source_name, author
     """
-    result = engine.execute(query).fetchall()
-    data = [{"source": row.source, "author": row.author, "count": row.count} for row in result]
+    with engine.connect() as con:
+        result = con.execute(text(query)).fetchall()
+    data = [{"source_name": row.source_name, "author": row.author, "count": row.count} for row in result]
     return jsonify(data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
